f0cb314a47b7966992c863341ee4b6c0eefc4404
Serge Kosachiov <skosachiov@gmail.com>
2025-12-15 09:48:27 +0300

    add clevis

--- assets/esl/generate-certs.sh
+++ assets/esl/generate-certs.sh
@@ -1,3 +1,5 @@
+# apt install efitools systemd-ukify python3-cryptography
+
 # Generate CA private key (PK)
 openssl req -newkey rsa:4096 -nodes -keyout PK.key -new -x509 -sha256 -days 3650 -subj "/CN=My Platform Key/" -out PK.crt
 # Generate KEK private key
